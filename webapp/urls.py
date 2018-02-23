980afc609a3c5995394d0a4f07821ec1b8699cdc
Javier Seixas <dev@javierseixas.com>
2018-02-23 12:19:11 -0300

    Refactors to have views as Class-Based

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.views.generic import ListView
 from .models import Repo
-from . import views
+from .views import ReposView, SearchView
 
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Repo.objects.all().order_by("-name")[:25], template_name='webapp/home.html')),
-    url(r'^repos/$', views.list),
-    url(r'^repos/search/', views.search),
+    url(r'^repos/$', ReposView.as_view()),
+    url(r'^repos/search/', SearchView.as_view()),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from . import services
 from .forms import SearchRepoForm
+from django.views.generic import View
 
 
-def index(request):
-    return HttpResponse("<h2>HEY!</h2>")
+class ReposView(View):
+    template_name = 'webapp/repos.html'
+
+    def get(self, request):
+        search_form = SearchRepoForm()
+        orderby = request.GET.get('orderby', 'name')
+        repos = services.get_repos(orderby=orderby)
+
+        return render(request, self.template_name, {'repos': repos, 'search_form': search_form})
 
 
-def list(request):
-    search_form = SearchRepoForm()
-    orderby = request.GET.get('orderby', 'name')
-    repos = services.get_repos(orderby=orderby)
-    return render(request, 'webapp/repos.html', {'repos': repos, 'search_form': search_form})
+class SearchView(View):
+    template_name = 'webapp/repos.html'
 
+    def get(self, request):
+        search_form = SearchRepoForm()
+        term = request.GET.get('term', '')
+        repos = services.search_repos(term)
 
-def search(request):
-    search_form = SearchRepoForm()
-    term = request.GET.get('term', '')
-    repos = services.search_repos(term)
-    return render(request, 'webapp/repos.html', {
-        'repos': repos,
-        'search_form': search_form,
-        'searched_term': term
-    })
+        return render(request, self.template_name, {
+            'repos': repos,
+            'search_form': search_form,
+            'searched_term': term
+        })
