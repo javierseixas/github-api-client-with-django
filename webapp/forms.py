b44f5a5914ed6950525c961899bb630d9efc512c
Javier Seixas <dev@javierseixas.com>
2018-02-25 18:04:33 -0300

    Applies bootstrap for frontend design

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class SearchRepoForm(forms.Form):
-    term = forms.CharField(50)
+    term = forms.CharField(50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Repository name'}))
--- webapp/urls.py
+++ webapp/urls.py
@@ -5,7 +5,6 @@ from .views import ReposView, SearchView
 
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Repo.objects.all().order_by("-name")[:25], template_name='webapp/home.html')),
     url(r'^repos/$', ReposView.as_view()),
     url(r'^repos/search/', SearchView.as_view()),
 ]
