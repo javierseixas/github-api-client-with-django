1bf7619e32587a06d46dbc4ded13679731bc2289
Javier Seixas <dev@javierseixas.com>
2018-02-25 19:27:35 -0300

    Fixes typo in build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@
 # Requires that a postgreSQL database has been created for the application
 #
 set -e # stops execution on error
-rm -rf forum/migrations
+rm -rf webapp/migrations
 python manage.py migrate
 python manage.py test
